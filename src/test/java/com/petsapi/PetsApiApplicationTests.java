ebc10a231996c9bed81d900c2f0fd198622065ca
phquartin <phquartinmatos@sempreceub.com>
2025-06-13 12:08:36 -0300

    init: Arquivos de inicialização do projeto.

---
+++ src/test/java/com/petsapi/PetsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
