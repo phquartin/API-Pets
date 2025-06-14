f369efaf5ad213688a2340ba7ee31e3ecaecf189
phquartin <phquartinmatos@sempreceub.com>
2025-06-13 22:48:04 -0300

    feat(pet): Adicionado funções de listar todos os pets e achar por id no service.

--- src/main/java/com/petsapi/service/PetService.java
+++ src/main/java/com/petsapi/service/PetService.java
@@ -1,10 +1,15 @@
 package com.petsapi.service;
 
 import com.petsapi.dto.pet.PetDTO;
+import com.petsapi.dto.pet.PetResponseDTO;
 import com.petsapi.mapper.PetMapper;
 import com.petsapi.model.pet.PetModel;
 import com.petsapi.repository.PetRepository;
+import com.petsapi.validator.pet.PetException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PetService {
@@ -19,6 +24,15 @@ public class PetService {
     public void save(PetDTO petDTO) {
         PetModel petModel = petMapper.toModel(petDTO);
         this.petRepository.save(petModel);
+    }
+
+    public PetResponseDTO getById(Long id) {
+        PetModel petModel = petRepository.findById(id).orElseThrow(() -> new PetException("Pet não encontrado"));
+        return petMapper.toResponseDTO(petModel);
+    }
+    public List<PetResponseDTO> getAll() {
+        List<PetModel> list = petRepository.findAll();
+        return list.stream().map(petMapper::toResponseDTO).collect(Collectors.toList());
     }
 
 }
